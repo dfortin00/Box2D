3310e41591d810e7999f34d26d309205dbcce6b6
dfortin00 <quadratonx@gmail.com>
2022-05-15 16:24:49 -0400

    Comment setup batch file

--- test/setup_test_lib.bat
+++ test/setup_test_lib.bat
@@ -1,5 +1,8 @@
 rmdir /Q /S lib\mapmatic
 rmdir /Q /S lib\mapmatic
 
+rem Modify source directory to the location of the MapMatic library before executing.
 xcopy ..\..\..\MapMatic\mapmatic  lib\mapmatic /E/H/C/I
+
+rem Copies the Box2D folder into the mapmatic/plugins folder for testing purposes.
 xcopy ..\Box2D  lib\mapmatic\plugins\Box2D /E/H/C/I
